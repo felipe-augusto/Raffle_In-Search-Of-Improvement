dd2931df35cd46248fc4523bd8edfd3707013594
Gabriel Feba <35573346+gabrielfeba@users.noreply.github.com>
2018-01-20 16:02:08 -0200

    simple adjusment
    I removed "canvas in 2d" and changed name.

--- raffle.js
+++ raffle.js
@@ -1,29 +1,27 @@
-var screen = document.getElementById("screen");
-var c = screen.getContext("2d");
 var drawm = 0;
-var yes = 0;
-var numbers = [];
-function raffle (X) {
-	drawn = Math.round(Math.random() * X);
+var hasClone = 0;
+function raffle (Length) {
+	drawn = Math.round(Math.random() * Length);
 };
-function verifyClone (X) {
-	for(var u = 0; u <= X.length; u++) {
-		if(X[u] == drawn) {
-			yes++;
+function verifyClone (whichArray) {
+	for(var u = 0; u <= whichArray.length; u++) {
+		if(whichArray[u] == drawn) {
+			hasClone++;
 		}
 	}
 };
 function loterry (amount,Length,whichArray) {
 	for(var i = 0; i < amount; i++) {
 		raffle(Length);
-		verifyClone(numbers);
-		if(yes == 0) {
+		verifyClone(whichArray);
+		if(hasClone == 0) {
 			numbers.push(drawn);
 		} else {
 			i--;
-			yes = 0;
+			hasClone = 0;
 		}
 	}
 };
-//Para sortear: loterry(QuantidadeDeNumeros, deZeroAtéQualNúmero, QualArray);
-//For loterry: loterry(AmountOfNumbers, zeroUntilTheNumber, whichArray);
+
+//Para sortear: crie uma array e dê loterry(QuantidadeDeNumeros, deZeroAtéQualNúmero, QualArray);
+//For loterry: create one array and give loterry(AmountOfNumbers, zeroUntilTheNumber, whichArray);
